be38bf5f6383927fcd689a420581b206758f2275
Ste Carlson <ste.carlson@gmail.com>
2019-08-21 22:28:12 -0500

    restructure and cleanup

--- sudoku.py
+++ sudoku.py
@@ -93,6 +93,12 @@ if DEBUG:
       idxs = get_indicies( idx )
       print_indicies( idxs )
 
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# make the game grid
+
 print "\nmake game"
 
 last_gx = 0
@@ -118,136 +124,177 @@ for idx in master_idx:
 
 
 
-   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+   # --------------------------------------------------------------------------------
 
+   # make all the data structures
+
+   choice = None
+   choose = xyg_curr
 
    # identify choices that can't go in the current spot because they have to go
    # in a different spot 
-   e_dict    = dict()
-   excl_list = set()
-
-
+   e_dict         = dict()
+   excl_list      = set()
 
    # create the dictionary that will collect the options for each of the
    # current choices
-   c2x_dict = dict()
+   c2x_dict       = dict()
    for c in xyg_curr:
       c2x_dict[c] = set()
 
    # create the dictionary that will collect the options for each of the
    # current choices
-   x2c_dict = dict()
-
+   x2c_dict       = dict()
 
    # create a grid to store the bucketized choices
-   c_opts = mk_grids()
-   c_cnt  = 0
+   c_opts         = mk_grids()
+   c_cnt          = 0
 
-   # if we're past the first row, but not on the last column
-   if y > 0 and x < 8:
+   path           = None
 
+   # --------------------------------------------------------------------------------
 
-      # peek ahead and gather statistics about the remaining choices
+   # look at the choices for the current square and decide what do do based on
+   # how many there are
 
-      for _x in range( x+1, 9 ):
+   # if the current square has no options, the game is broken; leave
+   # choice at None and we'll print an error below
+   if not xyg_curr:
 
+      path = 0
 
+   # if the current square has only one option, choose it and skip the the
+   # look-ahead
+   elif len( xyg_curr ) == 1:
 
-         # get the indicies of the given spot
-         _idxs = get_indicies_xy( _x, y )
-         _i, _x, _y, _gi, _gx, _gy, _gsx, _gsy = _idxs
+      path = 1
+      choice = list( xyg_curr )[0]
 
-         # get the column and grid ranks for the given spot
-         _x_temp  = x_ranks[ _x ]
-         _g_temp  = g_ranks[ _gi ]
+   # otherwise, we need to look ahead to constrain the choices we make for this
+   # square so we don't break the game
+   else:
 
-         # and take the instersection to see if any of the choice for the
-         # current spot are valid choiices for the given spot
-         xyg_peek   = set.intersection( xyg_curr, _x_temp, _g_temp )
-
-
-         if len( xyg_peek ) == 1:
-            c = list( xyg_peek )[0]
-            excl_list.add( c )
-
-
-         if xyg_peek:
-
-            x2c_dict[_x]  = xyg_peek
-
-            # for all of the valid choices, bump the counter in c2x_dict
-            for c in xyg_peek:
-               c2x_dict[c].add( _x )
+      path = 2
 
 
+      # ------------------------------------------------------------------
 
-      for c in excl_list:
-         e_dict[c] = c2x_dict[c]
-         del c2x_dict[c]
+      # if we're past the first row, but not on the last column, peek ahead and
+      # gather data about the options for the remaining squares on this row
 
+      if y > 0 and x < 8:
 
-      # now populate the bucketized choices
-      for c in c2x_dict.keys():
+         # peek ahead and gather statistics about the remaining choices
 
-         # for the current choice, find out how many options are left on the
-         # current row
-         _cnt = len( c2x_dict[c] )
-         # add the choice to the bucket for the given count
-         c_opts[ _cnt ].add( c )
-         c_cnt += 1
+         for _x in range( x+1, 9 ):
 
+            # get the indicies of the given spot
+            _idxs = get_indicies_xy( _x, y )
+            _i, _x, _y, _gi, _gx, _gy, _gsx, _gsy = _idxs
 
+            # get the column and grid ranks for the given spot
+            _x_temp  = x_ranks[ _x ]
+            _g_temp  = g_ranks[ _gi ]
 
-
-   choice = None
-
-   choose = set()
-   c_lvl  = None
-
-
-   # make sure the zero bucket is less than 2; this bucket contains those
-   # choices that have no other possible spots on this line if we have
-   # more than 1, then we can't complete this game grid
-   if len( c_opts[0] ) < 2:
-
-      if c_cnt:
-         for lvl in range( 0, len( c_opts ) ):
-            c_opt = c_opts[lvl]
-            if len( c_opt ):
-               choose = c_opt
-               c_lvl  = lvl
-               break
-
-      if not choose:
-         choose = xyg_curr
-         c_lvl  = None
+            # and take the instersection to see if any of the choice for the
+            # current spot are valid choiices for the given spot
+            xyg_peek   = set.intersection( xyg_curr, _x_temp, _g_temp )
 
 
+            # if there's only once choice for this square, then we need
+            # to exclude it from the current square
 
-   x_opts   = list()
-   x_choose = set()
+            if len( xyg_peek ) == 1:
+               c = list( xyg_peek )[0]
+               excl_list.add( c )
 
-   # for each choice in the final set, collect the set of columns for all of
-   # them
-   for c in choose:
-      # if the choice has an entry in the choice to column list
-      # TODO: why would a choice not have a list
-      if c2x_dict.has_key( c ):
-         x_opts.append( c2x_dict[c] )
+            # TODO; at one point, i made this an else if to skip this step if
+            # we're adding the item to exlustion list, but that seemed to break
+            # the game often enough that we never complete
 
-   # if we have column lists, generate the intersection
-   if x_opts:
-      x_choose = set.intersection( *x_opts )
+            if xyg_peek:
 
+               x2c_dict[_x]  = xyg_peek
 
-
-   if choose:
-      choice = random.choice( list( choose ) )
+               # for all of the valid choices, bump the counter in c2x_dict
+               for c in xyg_peek:
+                  c2x_dict[c].add( _x )
 
 
-   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+         # TODO: okay, do we have to do this here or can we just subtract the
+         # excl_list from the choose options below
+
+         for c in excl_list:
+            e_dict[c] = c2x_dict[c]
+            del c2x_dict[c]
 
 
+         # now populate the bucketized choices
+         for c in c2x_dict.keys():
+
+            # for the current choice, find out how many options are left on the
+            # current row
+            _cnt = len( c2x_dict[c] )
+            # add the choice to the bucket for the given count
+            c_opts[ _cnt ].add( c )
+            c_cnt += 1
+
+
+      # ------------------------------------------------------------------
+
+
+      c_lvl  = None
+
+      # make sure the zero bucket is less than 2; this bucket contains those
+      # choices that have no other possible spots on this line if we have
+      # more than 1, then we can't complete this game grid
+      if len( c_opts[0] ) < 2:
+
+         if c_cnt:
+            for lvl in range( 0, len( c_opts ) ):
+               c_opt = c_opts[lvl]
+               if len( c_opt ):
+                  choose = c_opt
+                  c_lvl  = lvl
+                  break
+
+
+      c_exp  = False
+
+      if c_lvl == 1 and len( choose ) > 2:
+
+         c_exp = True
+
+         # this is experimental code to try to address c_lvl == 1 cases where
+         # there are three choices
+
+         x_opts   = list()
+         x_choose = set()
+
+         # for each choice in the final set, collect the set of columns for all of
+         # them
+         for c in choose:
+            # if the choice has an entry in the choice to column list
+            # TODO: why would a choice not have a list
+            if c2x_dict.has_key( c ):
+               x_opts.append( c2x_dict[c] )
+
+         # if we have column lists, generate the intersection
+         if x_opts:
+            x_choose = set.intersection( *x_opts )
+
+
+
+      # make the actual choice
+
+      if choose:
+         choice = random.choice( list( choose ) )
+
+
+
+   # --------------------------------------------------------------------------------
+
+   # print out the debug spiffle
 
    if x == 0:
       print "\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
@@ -261,8 +308,9 @@ for idx in master_idx:
 
    print_indicies( idxs )
 
-   print "   ranks      = %s" % pprint.pformat( ranks,       indent=6 )
-   print "   xyg_curr   = %s" % pprint.pformat( xyg_curr,      indent=6 )
+   print "   ranks      = %s" % pprint.pformat( ranks,          indent=6 )
+   print "   xyg_curr   = %s" % pprint.pformat( xyg_curr,       indent=6 )
+   print "   path       = %d" % path
 
    if excl_list:
       print ""
@@ -270,27 +318,54 @@ for idx in master_idx:
       print "   e_dict     = %s" % pprint.pformat( e_dict,      indent=6 )
       print ""
 
-   print "   c2x        = %s" % pprint.pformat( c2x_dict,    indent=6 )
-   print "   x2c        = %s" % pprint.pformat( x2c_dict,    indent=6 )
-   print "   c_opts     = %s" % pprint.pformat( c_opts,      indent=6 )
-   print "   x_opts     = %s" % pprint.pformat( x_opts,      indent=6 )
-   print "   x_ch       = %s" % pprint.pformat( x_choose,    indent=6 )
-   print "   choose     = %s" % pprint.pformat( choose,      indent=6 )
+   print "   c2x        = %s" % pprint.pformat( c2x_dict,       indent=6 )
+   print "   x2c        = %s" % pprint.pformat( x2c_dict,       indent=6 )
+
+   if c_lvl is not None:
+      print "   c_lvl      = %d" % c_lvl
+
+   print "   c_opts     = %s" % pprint.pformat( c_opts,         indent=6 )
+
+   if c_exp:
+      print ""
+      print "   x_opts     = %s" % pprint.pformat( x_opts,      indent=6 )
+      print "   x_ch       = %s" % pprint.pformat( x_choose,    indent=6 )
+      print ""
+
+   print "   choose     = %s" % pprint.pformat( choose,         indent=6 )
+
+   print ""
+   print "   choice     = %s" % choice
+
+
+
+   # --------------------------------------------------------------------------------
+
+   # handle the results
 
    if choice:
+
+      # this add the current choice the result list
       result.append( choice )
+
+      # this updates all current ranks to remove the choice we made
       for r in ranks:
          r.remove( choice )
 
-   print "   ranks  = %s" % pprint.pformat( ranks, indent=6 )
-   print ""
-   print "   choice = %s" % choice
+   else:
 
-
-   if not choice:
+      # bark an error
       print "\n\n***** ERROR *****\n"
+      # and break the main loop that's iterating all the squares
       break
 
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# print out the results
+
+print "\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 
 last_gx = 0
 last_gy = 0
@@ -316,13 +391,9 @@ for idx in range( 0, len(result) ):
 
    print "%d" % result[ idx ],
 
-
 if len(result) == 81:
    print "\n\n***** SUCCESS *****\n"
    sys.exit(0)
 
 sys.exit(1)
-
-
-
 
--- unit_test.sh
+++ unit_test.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/zsh
 
-cnt=1
-while ! ./sudoku.py
-do
-   printf "\ncnt=%d\n" $((cnt++))
-done
-printf "\ncnt=%d\n" $cnt
+{
+
+   cnt=1
+   while ! ./sudoku.py
+   do
+      printf "\ncnt=%d\n" $((cnt++))
+   done
+   printf "\ncnt=%d\n" $cnt
+
+} | tee output.txt
 
